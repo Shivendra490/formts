7a515b0af07da6036d2cbcd17e744551835d7c39
Shivendra490 <shivendrasrivastava490@gmail.com>
2022-05-26 13:15:15 +0530

    issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link, Switch, useHistory, NavLink} from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch} from "react-router-dom";
 
 import Form from './components/Form';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
 
 
 
@@ -23,7 +24,8 @@ function App() {
 
                 <Switch>
                     <Route exact path="/" component={Form}/>
-                    <Route path="/login" component={Login}/>
+                    <Route  path="/login" component={Login}/>
+                    <Route  path="/welcome" component={Welcome}/>
                 </Switch>
             </BrowserRouter>
       
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,30 +1,68 @@
-
-import React, { useState } from 'react'
-import { Avatar, Button, Grid, Paper } from '@mui/material'
+import React, { useEffect, useState } from "react";
+import { Alert, Avatar, Button, Grid, Paper } from "@mui/material";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
+import { useNavigate } from "react-router-dom";
 
-
-const PaperStyle={
-    width:'30%',
-    margin:'3% auto'
-}
+const PaperStyle = {
+  width: "30%",
+  margin: "3% auto",
+};
 
 const avatarStyle = {
-    backGroundColor: "red",
-    color: "blue",
-    marginTop: "3%",
-  };
+  backGroundColor: "red",
+  color: "blue",
+  marginTop: "3%",
+};
 
-  interface loginDetails{
-    email:string
-    password:string
-  }
+interface loginDetails {
+  email: string;
+  password: string;
+}
+
+const keys = ["email", "password"];
 
 const Login = () => {
-  const [loginCred, setLoginCred] = useState<loginDetails>({email:"",password:""})
+  const [loginCred, setLoginCred] = useState<loginDetails>({
+    email: "",
+    password: "",
+  });
+
+  const history = useNavigate();
+
+  const [error, setError] = useState<loginDetails>({ email: "", password: "" });
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    const checkDisable =
+      keys.some((key) => loginCred[key as keyof loginDetails] === "") ||
+      keys.some((key) => error[key as keyof loginDetails] !== undefined);
+
+    setIsDisabled(checkDisable);
+  }, [error, loginCred]);
+
+  const history=useHistory()
+
+  const validate = (name: string, value: string) => {
+    switch (name) {
+      case "email":
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(value)) {
+          return "Please enter valid email";
+        }
+        break;
+
+      case "password":
+        if (value.length < 8) {
+          return "password should be greater than 8 chars";
+        }
+        break;
+    }
+    return;
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -32,73 +70,103 @@ const Login = () => {
     // send name of the field
     // if false return error else nothing
 
-    // const result = validate(name, value);
-    
-      // setError({ ...error, [name]: result });
-    
+    const result = validate(name, value);
+
+    setError({ ...error, [name]: result });
 
     setLoginCred({ ...loginCred, [name]: value });
   };
 
-  const handleSubmit=(e: React.MouseEvent<HTMLButtonElement>)=>{
-    e.preventDefault()
-    const currCred={...loginCred}
-
-    fetch(`https://sample-register.herokuapp.com/login`, {
-      body: JSON.stringify(currCred),
-      method: "post",
-      headers: { "Content-Type": "application/json" },// header is optional
-    })
-    .then((response) => response.json())
-    .then(data=>console.log(data))//response.data
-    .catch(error => console.log(error));
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
 
 
-  }
 
-  
+    try {
+      setIsDisabled(true);
 
- 
+      const response = await fetch(
+        `https://sample-register.herokuapp.com/login`,
+        {
+          body: JSON.stringify(loginCred),
+          method: "post",
+          headers: { "Content-Type": "application/json" }, // header is optional
+        }
+      );
+      const data = await response.json();
+
+
+      if (data && data.status === "success") {
+        history("/welcome");
+        setLoginCred({ email: "", password: "" });
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    
+
+    
+
+  };
 
   return (
-   <Grid>
-       <Paper elevation={10} style={PaperStyle}>
-       <Grid
+    <Grid>
+      <Paper elevation={10} style={PaperStyle}>
+        <Grid
           display="flex"
           justifyContent={"space-around"}
           alignContent={"center"}
         >
-          <h2 style={{marginTop:'0.7em'}}>Login</h2>
+          <h2 style={{ marginTop: "0.7em" }}>Login</h2>
           <Avatar style={avatarStyle}>
             <AddOutlinedIcon />
           </Avatar>
         </Grid>
         <Box
-      component="form"
-      sx={{
-        '& > :not(style)': {width: '100%' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField name="email" id="outlined-basic" label="Email" variant="outlined" onChange={handleChange} style={{margin:'2% 0'}}/>
-      <TextField name="password" id="outlined-basic" label="Password" variant="outlined" onChange={handleChange}/>
-      <Button
+          component="form"
+          sx={{
+            "& > :not(style)": { width: "100%" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            name="email"
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            onChange={handleChange}
+            style={{ margin: "2% 0" }}
+            value={loginCred.email}
+          />
+          <div style={{ width: "100%" }}>
+            {error.email && <Alert severity="error">{error.email}</Alert>}
+          </div>
+          <TextField
+            name="password"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            onChange={handleChange}
+            value={loginCred.password}
+          />
+          <div style={{ width: "100%" }}>
+            {error.password && <Alert severity="error">{error.password}</Alert>}
+          </div>
+          <Button
             type="submit"
-            
             variant="contained"
             style={{ margin: "1% 0" }}
             onClick={handleSubmit}
-            
+            disabled={isDisabled}
           >
             Login
           </Button>
+        </Box>
+      </Paper>
+    </Grid>
+  );
+};
 
-    </Box>
-
-       </Paper>
-   </Grid>
-  )
-}
-
-export default Login
+export default Login;
